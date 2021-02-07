f3cab78c891856a22f65fec3b0415e11a62276e3
Neven <58472164+neven14@users.noreply.github.com>
2021-02-07 15:58:57 +0100

    Add files via upload

---
+++ MonthCalenderLontrola/MonthCalenderLontrola/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthCalenderLontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan ts = monthCalendar1.SelectionEnd - monthCalendar1.SelectionStart;
+            if (ts.Days < 1)
+            {
+                MessageBox.Show("Korištenjem tipke SHIFT odaberite opseg datuma.");
+                return;
+            }
+            MessageBox.Show("Vasa rezervacija počinje" + monthCalendar1.SelectionStart.ToShortDateString() + "a završava se" + monthCalendar1.SelectionEnd.ToShortDateString());
+        }
+    }
+}
